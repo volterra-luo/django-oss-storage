9641272ac08fdef16ef19d26219f9340d10b5313
Volterra <volterra.luo@gmail.com>
2014-11-06 00:52:41 +0800

    init tests.py

---
+++ storages/tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from django.test.utils import get_runner
+
+BASE_PATH = os.path.dirname(__file__)
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+    main()
