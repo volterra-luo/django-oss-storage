c373ea1b1216af611d74107800317689bbd380d1
volterra <volterra.luo@gmail.com>
2014-11-07 20:05:06 +0800

    init tests.py

--- tests.py
+++ tests.py
@@ -8,12 +8,11 @@ import django
 BASE_PATH = os.path.dirname(__file__)
 
 def main():
-
 	os.environ["DJANGO_SETTINGS_MODULE"] = "django.conf.global_settings"
 	from django.conf import global_settings
 
 	from django.test.utils import get_runner
-    test_runner = get_runner(global_settings)
+	test_runner = get_runner(global_settings)
 
 if __name__ == '__main__':
     main()
