1847a2db73e769a4234cd3af0a66bca432519289
volterra-luo <volterra-luo@gmail.com>
2014-11-09 10:59:27 +0800

    add tests.py

--- tests.py
+++ tests.py
@@ -12,6 +12,13 @@ def main():
 	os.environ["DJANGO_SETTINGS_MODULE"] = "django.conf.global_settings"
 	from django.conf import global_settings
 
+	global_settings.INSTALLED_APPS = (
+        'django.contrib.auth',
+        'django.contrib.sessions',
+        'django.contrib.contenttypes',
+        'storages',
+    )
+
 	from django.test.utils import get_runner
 	test_runner = get_runner(global_settings)
 
