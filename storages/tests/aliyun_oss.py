14ddacc60d0f4f7032e8f6c7175c8d6dc72681d4
luocheng <cheng.luofd@gmail.com>
2014-11-12 23:12:38 +0800

    minor

--- storages/tests/aliyun_oss.py
+++ storages/tests/aliyun_oss.py
@@ -15,11 +15,27 @@ from django.core.files.storage import FileSystemStorage
 
 from storages.backends import aliyun_oss
 
-
 class AliyunOssTestCase(TestCase):
-    @mock.patch('')
-    def setUp(self):
-        self.storage = aliyun_oss.S3BotoStorage()
+	
+    def test_storage_save(self):
+        """
+        Test saving a file
+        """
+        name = 'test_storage_save.txt'
+        content = ContentFile('new content')
+        self.storage.save(name, content)
+        self.storage.bucket.get_key.assert_called_once_with(name)
+        
+        key = self.storage.bucket.get_key.return_value
+        key.set_metadata.assert_called_with('Content-Type', 'text/plain')
+        key.set_contents_from_file.assert_called_with(
+            content,
+            headers={},
+            policy=self.storage.acl,
+            reduced_redundancy=self.storage.reduced_redundancy,
+        )
+
+
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
